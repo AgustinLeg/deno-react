5ecc434c0cb9aaa1823447e40f2fef94097c4f31
Agustin Leguizamon <agustin.leg7@gmail.com>
2022-08-01 01:03:52 -0300

    delete files

--- deps.ts
+++
@@ -1,6 +0,0 @@
-export { default as ReactDOMServer } from "https://esm.sh/react-dom@18.2.0/server";
-export { default as React } from "https://esm.sh/react@18.2.0";
-export {
-  createApp,
-  contentTypeFilter,
-} from "https://deno.land/x/servest@v1.3.1/mod.ts";
